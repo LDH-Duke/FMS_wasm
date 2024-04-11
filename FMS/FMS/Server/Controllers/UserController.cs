f55e3a9a0e24c81900391372e6e093e3d3910f3c
DUKE <dukeldh1128@gmail.com>
2024-04-11 22:18:01 +0900

    feat : server structure setting [controller, service, repository, model, connection DB]

---
+++ FMS/FMS/Server/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using FMS.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMS.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// 회원 가입
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult AddUser(Users user)
+        {
+            //서비스 호출
+            Console.WriteLine(user);
+            return Ok(user);
+        }
+
+        /// <summary>
+        /// 단일 조회
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetUserByAccount(string account)
+        {
+            //서비스 호출
+            Console.WriteLine(account);
+            return Ok(account);
+        }
+
+        /// <summary>
+        /// 전체 조회
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            //서비스 호출
+            
+            return Ok();
+        }
+
+        /// <summary>
+        /// 회원 수정
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult EditUser(Users user)
+        {
+            //서비스 호출
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// 회원 삭제
+        /// </summary>
+        /// <param name="Account"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult DeleteUser(string account)
+        {
+            //서비스 호출
+
+            return Ok();
+        }
+    }
+}
---
+++ FMS/FMS/Shared/Models/Users.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Shared.Models
+{
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Account { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? PhoneNumber { get; set; }
+    }
+}
