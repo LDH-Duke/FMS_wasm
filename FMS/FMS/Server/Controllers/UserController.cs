c12b5ace0ab36ac15bceb33666c67540ee31467d
duke <ehdgml506@naver.com>
2024-04-17 13:12:10 +0900

    Revert "Merge branch 'main' of https://github.com/LDH-Duke/FMS_wasm into main"
    This reverts commit 3f51db4906d59e0a9e37b7c9ea0268aecbebff95, reversing
    changes made to f903a0f70b541d1086b998651eb8f247d7fd30b1.

---
+++ FMS/FMS/Server/Controllers/UserController.cs
@@ -0,0 +1,122 @@
+﻿using FMS.Server.Services.UserService;
+using FMS.Server.Services.UserService.DTOs;
+using FMS.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMS.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserServiceAsync _userService;
+
+        public UserController(IUserServiceAsync userService)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// 회원 가입
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> AddUser(UserDTO userDTO)
+        {
+            try
+            {
+                //서비스 호출
+                Console.WriteLine(userDTO);
+                await _userService.AddUserAsync(userDTO);
+                return Ok(userDTO);
+            }
+            catch (Exception ex)
+            {
+                return Ok();
+            }
+        }
+
+        /// <summary>
+        /// 단일 조회
+        /// </summary>
+        /// <param name="account"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetUserByAccount(string account)
+        {
+            try
+            {
+                //서비스 호출
+                Console.WriteLine(account);
+                return Ok(account);
+            }
+            catch (Exception ex)
+            {
+                return Ok();
+            }
+            
+        }
+
+        /// <summary>
+        /// 전체 조회
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetAllUser()
+        {
+            try
+            {
+                //서비스 호출
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return Ok();
+            }
+            
+        }
+
+        /// <summary>
+        /// 회원 수정
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult EditUser(Users user)
+        {
+            try
+            {
+                //서비스 호출
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return Ok();
+
+            }
+            
+        }
+
+        /// <summary>
+        /// 회원 삭제
+        /// </summary>
+        /// <param name="Account"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult DeleteUser(string account)
+        {
+            try
+            {
+                return Ok();
+            }catch(Exception ex)
+            {
+                return Ok();
+            }
+        }
+    }
+}
---
+++ FMS/FMS/Server/Services/UserService/UserService.cs
@@ -0,0 +1,61 @@
+﻿using FMS.Server.Repository.UserRepository;
+using FMS.Server.Services.UserService.DTOs;
+using FMS.Shared.Models;
+
+namespace FMS.Server.Services.UserService
+{
+    public class UserService : IUserServiceAsync
+    {
+        private readonly IUserRepositoryAsync _userRepositoryAsync;
+
+        public UserService(IUserRepositoryAsync userRepositoryAsync)
+        {
+            _userRepositoryAsync = userRepositoryAsync;
+        }
+
+        public async Task<Users> AddUserAsync(UserDTO userDto)
+        {
+            try
+            {
+                Console.WriteLine("service");
+                Console.WriteLine(userDto);
+                //await _userRepositoryAsync.Add(userDto);
+
+                return new Users();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[UserService][AddUser] User Service 사용자 추가 에러 ! " + ex);
+                throw ex;
+            }
+        }
+
+        public Task<LoginDTO> LoginAsync(LoginDTO loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<List<Users>> GetAllUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Users> GetUserByAccountAsync(string account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(string account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Users> EditAsync(UserDTO userDto)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
